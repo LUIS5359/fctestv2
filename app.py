03f5b6a60c72a130f3e68276aa50b886abae8e9c
LUIS5359 <az.l.p.a.g@gmail.com>
2026-03-17 20:38:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -347,5 +347,5 @@ def _interpretar_pago_parcial(estado: str, valor: str) -> float:
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
